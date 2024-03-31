6761b5a0008b36b7804cad1ae8a199328b33fb9d
anisahalfianap <anisahalfiana@gmail.com>
2024-03-31 17:17:04 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 client = MongoClient('mongodb+srv://anisah:user@cluster0.ikc58s9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client.db_sparta2
